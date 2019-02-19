0ccd4306be20269fd075e2329c8d78f99d7d3e28
秦兴汉 <sean@arempel.u17.stevens-tech.edu>
2019-02-19 18:23:21 -0500

    modifying different classes

--- Billper/Group.py
+++ Billper/Group.py
@@ -2,22 +2,21 @@
 
 
 class Group:
-    __slots__ = ("group_num", "group_name", "size", "term", "balance", "users", "group_holder")
+    __slots__ = ("group_num", "group_name", "size", "size_used", "term", "balance", "users", "group_holder")
 
     temperate_group_num = 1
 
     def __init__(self, group_name, size, term, user):
+        if Group.temperate_group_num >= 999999:
+            print("The size is full!")
+            return
         self.group_name = group_name
         self.size = size
         self.term = term
         self.users = dict()
+        self.size_used = 0
         self.group_holder = user
-        num = str(self.temperate_group_num)
-        length = 6 - len(num)
-        temp = ""
-        for i in range(length):
-            temp += "0"
-        self.group_num = temp + num
+        self.group_num = "{:06}".format(Group.temperate_group_num)
         Group.temperate_group_num += 1
 
     def get_group_name(self):
@@ -43,7 +42,11 @@ class Group:
         if user.get_username() in self.users:
             print("This user has already been in the group")
             return
+        if self.size_used >= self.size:
+            print("The group is full")
+            return
         self.users[user.username] = user
+        self.size_used += 1
         user.add_group(self.group_num, self)
 
     def get_user(self, username):
@@ -61,9 +64,11 @@ class Group:
         user_name = user.get_username()
         if user_name in self.users:
             self.users.pop(user_name)
+            self.size_used -= 1
             user.delete_group(self.group_num)
             print(user_name, "deleted")
         else:
             print("This user is not in the group!")
             return
 
+ #   def create_add_member_request(self, user):
